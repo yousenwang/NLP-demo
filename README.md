bb85b75d2a67ce9294c56e908cd6b2b012f680de
Ethan <ethan.ys.wang@fii-foxconn.com>
2023-05-03 16:52:12 +0800

    fastAPI

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # Information Retrieval
+
+
+## Setup
+
+```
+python -m venv venv
+```
+
+```
+.\venv\Scripts\Activate.ps1
+```
+
+```
+pip install -r requirements.txt
+```
+
+## In the terminal, run
+
+```
+uvicorn main:app --reload
+```
+
+`filename`(main):`fastapi_instance_name` (app)
+
+## Postman
+
+### Input
+POST localhost:8000
+
+Body raw JSON
+
+```json
+{
+    "user_input": "What is the work station?"
+}
+```
+
+### Output
+
+```json
+{
+    "user_input": "What is the work station?",
+    "distance": 1.167005873213956,
+    "preidcted_q": "How to set the products and stations that operators can work on?",
+    "preidcted_ans": "Service center background -> Service Settings -> Station Settings, select the operator, click [product Settings] to check the product, you can set the operator can work on the product; Click `Station Settings` to check `station Settings` to set the station that the operator can operate."
+}
+```
---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+# import nltk
+# import sklearn
+
+app = FastAPI()
+
+
+class Question(BaseModel):
+    user_input:str
+
+
+# repalce with database
+faq = pd.read_csv('english_FAQ.csv')
+
+str_to_numerical = joblib.load('trained_estimators/trained_tfidf_vectorizer.pkl')
+info_retrieve = joblib.load('trained_estimators/trained_kd_tree.pkl')
+
+@app.post("/")
+async def answer_endpoint(input_q: Question):
+    question = input_q.dict()
+    numerical = str_to_numerical.transform([question["user_input"]]).toarray()
+    distance, idx = info_retrieve.query(numerical, k=1)
+
+    best_answer = list(enumerate(idx[0]))[0]
+
+    return {
+        "user_input": question["user_input"],
+        "distance": distance[0][best_answer[0]],
+        "preidcted_q": faq["question"][best_answer[1]],
+        "preidcted_ans": faq["reply"][best_answer[1]],
+    }
